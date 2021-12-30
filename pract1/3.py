c6ea1a8a86b679a5eb01b4159f1d4682fdc8b4fb
Vasiliy <vasiokpavluyk@gmail.com>
2021-12-30 10:33:29 +0200

    final

--- pract1/3.py
+++ pract1/3.py
@@ -9,19 +9,19 @@ def check(parse, i, is_formula):
             return False
 
     if parse[i] not in '+-' and not parse[i].isdigit():
-        isFormula = False
+        is_formula = False
 
     i += 1
-    return check(parse, i, isFormula)
+    return check(parse, i, is_formula)
 
 def main():
     parse = "".join(sys.argv[1:])
     if not len(parse) or not parse[0].isdigit() or not parse[len(parse) - 1].isdigit():
-        isRight = False
+        is_right = False
     else:
-        isRight = check(parse, 0, True)
+        is_right = check(parse, 0, True)
 
-    if isRight:
+    if is_right:
         print("True,", eval(parse))
     else:
         print("False, None")
